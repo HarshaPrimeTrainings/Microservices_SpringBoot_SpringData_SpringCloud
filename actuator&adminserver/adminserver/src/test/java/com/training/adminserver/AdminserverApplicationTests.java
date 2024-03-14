1c9c5fddfc91df14df01354ac0e183b568c8810a
HarshaVardhan <athaluri.harsha@gmail.com>
2024-03-14 08:27:46 +0530

    actuator

---
+++ actuator&adminserver/adminserver/src/test/java/com/training/adminserver/AdminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
